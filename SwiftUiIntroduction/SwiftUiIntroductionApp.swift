409a33f387102dc1cafcd6f35141a0a9b6515636
Dmitrii Dudkin <ddudkin@findmykids.org>
2024-12-27 22:05:46 +0100

    Initial Commit

---
+++ SwiftUiIntroduction/SwiftUiIntroductionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiIntroductionApp.swift
+//  SwiftUiIntroduction
+//
+//  Created by ddudkin on 27.12.24..
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiIntroductionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
