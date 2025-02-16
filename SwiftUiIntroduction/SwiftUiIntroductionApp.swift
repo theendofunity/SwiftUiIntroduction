34fba72173c8a39e025294849d65eafe148f14c8
Dmitrii Dudkin <ddudkin@findmykids.org>
2025-02-16 16:46:13 +0100

    make custom notifications

--- SwiftUiIntroduction/SwiftUiIntroductionApp.swift
+++ SwiftUiIntroduction/SwiftUiIntroductionApp.swift
@@ -16,5 +16,9 @@ struct SwiftUiIntroductionApp: App {
             ContentView()
                 .environment(data)
         }
+        
+#if os(watchOS)
+        WKNotificationScene(controller: NotificationController.self, category: "LandmarkNear")
+#endif
     }
 }
--- WatchLandmarks Watch App/ContentView.swift
+++ WatchLandmarks Watch App/ContentView.swift
@@ -6,10 +6,15 @@
 //
 
 import SwiftUI
+import UserNotifications
 
 struct ContentView: View {
     var body: some View {
         LandmarkList()
+            .task {
+                let center = UNUserNotificationCenter.current()
+                _ = try? await center.requestAuthorization(options: [.badge, .alert, .sound])
+            }
     }
 }
 
